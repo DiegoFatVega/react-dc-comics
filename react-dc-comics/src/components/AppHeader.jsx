211f4fd3345fdc863faaeac3917422240fcfbb5f
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-22 15:41:49 +0200

    add Bootstrap and Sass dependencies; update AppHeader component layout

--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -1,7 +1,24 @@
+import dcLogo from '../assets/img/dc-logo.png'
 export default function AppHeader() {
     return (
         <header>
-
+            <div className="container">
+                <div className="row">
+                    <img src={dcLogo} alt="" className="col" />
+                    <nav className="col">
+                        <a><b>CHARACTERS</b></a>
+                        <a><b>COMICS</b></a>
+                        <a><b>MOVIES</b></a>
+                        <a><b>TV</b></a>
+                        <a><b>GAMES</b></a>
+                        <a><b>COLLECTIBLES</b></a>
+                        <a><b>VIDEOS</b></a>
+                        <a><b>FANS</b></a>
+                        <a><b>NEWS</b></a>
+                        <a><b>SHOP</b></a>
+                    </nav>
+                </div>
+            </div>
         </header>
     )
 }
--- react-dc-comics/src/main.jsx
+++ react-dc-comics/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Supports weights 300-800
+import '@fontsource-variable/open-sans';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
