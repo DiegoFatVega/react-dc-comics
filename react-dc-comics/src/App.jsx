b4ac1868441be65946c07d6dc9ded1d397e81f78
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-22 14:40:51 +0200

    structure app layout and remove unused CSS file.

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,12 +1,19 @@
 import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg'
 
+import AppHeader from './components/appHeader'
+import AppMain from './components/appMain'
+import AppFooter from './components/AppFooter'
 function App() {
 
 
   return (
     <>
+      <AppHeader />
 
+      <AppMain />
+
+      <AppFooter />
     </>
   )
 }
---
+++ react-dc-comics/src/components/AppMain.jsx
@@ -0,0 +1,7 @@
+export default function AppMain() {
+    return (
+        <main>
+            <h1>welcome to my app</h1>
+        </main>
+    )
+}
