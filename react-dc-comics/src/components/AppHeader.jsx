4d86fe7c36495807c0e63a5c16ad58919c7d85ec
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-23 23:17:46 +0200

    refactor AppHeader component; update layout and styles, remove unused styles.scss and bootstrap

--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -1,22 +1,21 @@
 import dcLogo from '../assets/img/dc-logo.png'
+
 export default function AppHeader() {
     return (
         <header>
-            <div className="">
-                <div className="row">
-                    <img src={dcLogo} alt="" className="col-1" />
-                    <nav className="col text-end mt-5">
-                        <a><b>CHARACTERS</b></a>
-                        <a><b>COMICS</b></a>
-                        <a><b>MOVIES</b></a>
-                        <a><b>TV</b></a>
-                        <a><b>GAMES</b></a>
-                        <a><b>COLLECTIBLES</b></a>
-                        <a><b>VIDEOS</b></a>
-                        <a><b>FANS</b></a>
-                        <a><b>NEWS</b></a>
-                        <a><b>SHOP</b></a>
-                    </nav>
+            <div className="container d-flex nav-bar">
+                <img src={dcLogo} alt="DC-logo" className="dc-logo" />
+                <div className="container d-flex options">
+                    <a><b>CHARACTERS</b></a>
+                    <a><b>COMICS</b></a>
+                    <a><b>MOVIES</b></a>
+                    <a><b>TV</b></a>
+                    <a><b>GAMES</b></a>
+                    <a><b>COLLECTIBLES</b></a>
+                    <a><b>VIDEOS</b></a>
+                    <a><b>FANS</b></a>
+                    <a><b>NEWS</b></a>
+                    <a><b>SHOP</b></a>
                 </div>
             </div>
         </header>
--- react-dc-comics/src/main.jsx
+++ react-dc-comics/src/main.jsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // Supports weights 300-800
-// Import our custom CSS
-import './styles.scss'
-
-// Import all of Bootstrap’s JS
-import * as bootstrap from 'bootstrap'
 import '@fontsource-variable/open-sans';
 import App from './App.jsx'
 
