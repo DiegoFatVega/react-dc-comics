6018d42f2a506ae290852e04b3b5af3d3916c04e
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-27 16:58:44 +0200

    refactor: changed header links unsing prop

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,18 +4,20 @@ import HeaderLinks from './HeaderLinks'
 export default function AppHeader() {
     return (
         <header>
-            <div className="d-flex nav-bar">
+            <div className="container d-flex nav-bar">
                 <img src={dcLogo} alt="DC-logo" />
-                <HeaderLinks link={'CHARACTERS'}></HeaderLinks>
-                <HeaderLinks link={'COMICS'}></HeaderLinks>
-                <HeaderLinks link={'MOVIES'}></HeaderLinks>
-                <HeaderLinks link={'TV'}></HeaderLinks>
-                <HeaderLinks link={'GAMES'}></HeaderLinks>
-                <HeaderLinks link={'COLLECTIBLES'}></HeaderLinks>
-                <HeaderLinks link={'VIDEOS'}></HeaderLinks>
-                <HeaderLinks link={'FANS'}></HeaderLinks>
-                <HeaderLinks link={'NEWS'}></HeaderLinks>
-                <HeaderLinks link={'SHOP'}></HeaderLinks>
+                <div className='d-flex options'>
+                    <HeaderLinks link={'CHARACTERS'}></HeaderLinks>
+                    <HeaderLinks link={'COMICS'}></HeaderLinks>
+                    <HeaderLinks link={'MOVIES'}></HeaderLinks>
+                    <HeaderLinks link={'TV'}></HeaderLinks>
+                    <HeaderLinks link={'GAMES'}></HeaderLinks>
+                    <HeaderLinks link={'COLLECTIBLES'}></HeaderLinks>
+                    <HeaderLinks link={'VIDEOS'}></HeaderLinks>
+                    <HeaderLinks link={'FANS'}></HeaderLinks>
+                    <HeaderLinks link={'NEWS'}></HeaderLinks>
+                    <HeaderLinks link={'SHOP'}></HeaderLinks>
+                </div>
             </div>
         </header>
     )
--- src/components/HeaderLinks.jsx
+++ src/components/HeaderLinks.jsx
@@ -2,8 +2,6 @@
 export default function HeaderLinks({ link }) {
 
     return (
-        <div className="d-flex options">
-            <a><b>{link}</b></a>
-        </div>
+        <a><b>{link}</b></a>
     )
 }
